4c03ede6f5822e4ee2e0b5f8d60f367cc7ca7a1f
NechyporYurii <58764637+NechyporYurii@users.noreply.github.com>
2021-02-13 16:45:32 +0200

    Update TheEighthTask.java

--- src/TheFirstApp/TheEighthTask.java
+++ src/TheFirstApp/TheEighthTask.java
@@ -1,7 +1,17 @@
 package TheFirstApp;
 
-public class TheEighthTask {
-    public static void main(String[] args) {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class TheEightTask {
+    public static void main(String[] args) {
+        String password = "sfsdsfdfsfds_D345";
+        String regex = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9_]{8,}";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(password);
+
+        if (m.matches()) {
+            System.out.println("Пароль правильний");
+        } else System.out.println("Пароль не правильний");
     }
 }
