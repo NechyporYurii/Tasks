880f6e8842d031063b2b9350565d7df9238dc4c8
NechyporYurii <58764637+NechyporYurii@users.noreply.github.com>
2021-02-13 16:41:23 +0200

    Update TheSecondAndTheThirdTask.java

--- src/TheFirstApp/TheSecondAndTheThirdTask.java
+++ src/TheFirstApp/TheSecondAndTheThirdTask.java
@@ -4,26 +4,25 @@ import java.util.Arrays;
 
 public class TheSecondAndTheThirdTask {
     public static void main(String[] args) {
-        int[] array = new int [] {6, 2, 4, 17, 5, 15, 7, 8, 9};
+        int[] array = {6, 2, 4, 17, 5, 15, 7, 8, 9};
         int sum = 0, min = array[0], max  = array[0];
 
-        for (int i = 0; i < array.length; i++)
-        {
+        for (int i = 0; i < array.length; i++) {
             sum += array[i];
-            if (min > array[i])
-            {
+
+            if (min > array[i]) {
                 min = array[i];
             }
-            if (max < array[i])
-            {
+
+            if (max < array[i]) {
                 max = array[i];
             }
         }
+
         System.out.println("Сума елементів масива = " + sum);
         System.out.println("Мінімальний елемент = " + min);
         System.out.println("Максимальний елемент = " + max);
-
         System.out.println("Мінімальний елемент = " + Arrays.stream(array).min().getAsInt());
-        System.out.println("Максимальний елемент = " + Arrays.stream(array).min().getAsInt());
+        System.out.println("Максимальний елемент = " + Arrays.stream(array).max().getAsInt());
     }
 }
