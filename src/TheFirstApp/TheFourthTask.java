bea1abe3aba038e47a5b00c2555e5ca2e892308a
NechyporYurii <58764637+NechyporYurii@users.noreply.github.com>
2021-02-13 16:41:51 +0200

    Update TheFourthTask.java

--- src/TheFirstApp/TheFourthTask.java
+++ src/TheFirstApp/TheFourthTask.java
@@ -1,20 +1,29 @@
 package TheFirstApp;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TheFourthTask {
     public static void main(String[] args) {
-        int[] array1 = new int [] {6, 2, 4, 17, 5, 15, 7, 8, 9};
-        int[] array2 = new int [] {3, 2, 4, 15, 8, 11, 1, 10, 9};
+        int[] array1 = new int [] {6, 2, 4, 17, 5, 15, 7, 8, 9, 20};
+        int[] array2 = new int [] {20, 3, 2, 4, 15, 8, 11, 1, 10, 9};
         List<Integer> commonElements = new ArrayList<Integer>();
 
-        for (int i = 0; i < array1.length; i++)
-            for (int j = 0; j < array2.length; j++)
-            {
-                if(array1[i] == array2[j])
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i] == array2[j]) {
                     commonElements.add(array1[i]);
+                }
             }
+        }
+
         System.out.println("Спільні елементи = " + commonElements);
+
+        List<Integer> listOfArray1 = Arrays.stream(array1).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        List<Integer> listOfArray2 = Arrays.stream(array2).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        listOfArray1.retainAll(listOfArray2);
+
+        System.out.println("Спільні елементи = " + listOfArray1);
     }
 }
