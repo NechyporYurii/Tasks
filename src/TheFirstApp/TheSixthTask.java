257f5c5f4e263348b983465a45d0da8167731eb1
NechyporYurii <58764637+NechyporYurii@users.noreply.github.com>
2021-02-13 16:44:38 +0200

    Update TheSixthTask.java

--- src/TheFirstApp/TheSixthTask.java
+++ src/TheFirstApp/TheSixthTask.java
@@ -3,10 +3,26 @@ package TheFirstApp;
 public class TheSixthTask {
     public static void main(String[] args) {
         String sentence = "I have two dogs. The dogs are in white and black color. I love my dogs.";
-        String space = " ";
-        String empty = "";
+        String string_space = " ";
+        String string_empty = "";
+        String newString = "";
+        char char_space = ' ';
+        char char_empty = 0;
 
         System.out.println("Original String is: \"" + sentence + "\"");
-        System.out.println("String after replacing is: \"" + sentence.replace(space, empty) + "\"");
+        System.out.println("String after replacing is: \"" + sentence.replace(string_space, string_empty) + "\"");
+
+        for (int i = 0, n = sentence.length(); i < n; i++) {
+            char c = sentence.charAt(i);
+
+            if (c == char_space) {
+                c = char_empty;
+            }
+
+            String character = Character.toString(c);
+            newString += character;
+        }
+
+        System.out.println("String after replacing is: \"" + newString + "\"");
     }
 }
